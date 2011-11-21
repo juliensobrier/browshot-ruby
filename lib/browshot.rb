a786dbc3ae2425a3b417cba9562945075cce7469
Julien Sobrier <julien@sobrier.net>
2011-11-20 17:39:17 -0800

    Inline documentation

--- lib/browshot.rb
+++ lib/browshot.rb
@@ -1,60 +1,100 @@
+# The library requires an API key from Browshot.
+# Sign up fpr a free account a http://browshot.com/
+# 
+# See README.rdoc for more information abouth Browshot
+# and this library.
+#
+# Author::    Julien Sobrier  (mailto:jsobrier@browshot.com)
+# Copyright:: Copyright (c) 2011 Browshot
+# License::   Distributes under the same terms as Ruby
+
 require 'url'
 require 'json'
 require 'net/http'
 require 'net/https'
 
 class Browshot
-	attr_reader :key, :base, :debug
+	# API key
+	attr_reader :key
+	# Base URL for all API requests. You should use the default base provided by the library. Be careful if you decide to use HTTP instead of HTTPS as your API key could be sniffed and your account could be used without your consent.
+	attr_reader :base
+	# print debug output to the standard output
+	attr_reader :debug
 
+	# New client
+	# 
+	# +key+:: API key
+	# +base+:: Base URL for all API requests. You should use the default base provided by the library. Be careful if you decide to use HTTP instead of HTTPS as your API key could be sniffed and your account could be used without your consent.
+	# +debug+:: Set to true to print debug output to the standard output. false (disabled) by default.
 	def initialize(key='', base='https://api.browshot.com/api/v1/', debug=false)
 		@key = key || ''
 		@base = base || 'http://127.0.0.1:3000/api/v1/'
 		@debug = debug || false
 	end
 
+	# Return the API version handled by the library. Note that this library can usually handle new arguments in requests without requiring an update.
 	def api_version()
 		return "1.2"
 	end
 
+	# Return the list of instances. See http://browshot.com/api/documentation#instance_list for the response format.
 	def instance_list()
 		return return_reply('instance/list')
 	end
 
+	# Return the details of an instance. See http://browshot.com/api/documentation#instance_info for the response format.
+	#
+	# +id+:: Instance ID
 	def instance_info(id=0)
 		return return_reply('instance/info', { 'id' => id })
 	end
 
+	# Create a private instance. See http://browshot.com/api/documentation#instance_create for the response format.
 	def instance_create(parameters={})
 		return return_reply('instance/create', parameters)
 	end
 
-
+	# Return the list of browsers. See http://browshot.com/api/documentation#browser_list for the response format.
 	def browser_list()
 		return return_reply('browser/list')
 	end
 
+	# Return the details of a browser. See http://browshot.com/api/documentation#browser_info for the response format.
+	#
+	# +id+:: Browser ID
 	def browser_info(id=0)
 		return return_reply('browser/info', { 'id' => id })
 	end
 
+	# Create a custom browser. See http://browshot.com/api/documentation#browser_create for the response format.
 	def browser_create(parameters={})
 		return return_reply('browser/create', parameters)
 	end
 
-
+	# Request a screenshot. See http://browshot.com/api/documentation#screenshot_create for the response format.
+	#
+	# +url+:: URL of the website to create a screenshot of.
 	def screenshot_create(url='', parameters={})
 		parameters[:url] = url
 		return return_reply('screenshot/create', parameters)
 	end
 
+	# Get information about a screenshot requested previously. See http://browshot.com/api/documentation#screenshot_info for the response format.
+	#
+	# +id+:: screenshot ID
 	def screenshot_info(id=0)
 		return return_reply('screenshot/info', { 'id' => id })
 	end
 
+	# Get details about screenshots requested. See http://browshot.com/api/documentation#screenshot_list for the response format.
 	def screenshot_list(parameters={})
 		return return_reply('screenshot/list', parameters)
 	end
 
+	# Retrieve the screenshot, or a thumbnail. See http://browshot.com/api/documentation#thumbnails for the response format.
+	#
+	# Return an empty string if the image could not be retrieved.
+	# +id+:: screenshot ID
 	def screenshot_thumbnail(url='', parameters={})
 		begin
 			url =  URL.new(url)
@@ -80,10 +120,12 @@ class Browshot
 		end
 	end
 
-	def account_info(parameters={})
-		return return_reply('account/info', parameters)
-	end
-
+	# Retrieve the screenshot, or a thumbnail, and save it to a file. See http://browshot.com/api/documentation#thumbnails for the response format.
+	#
+	# See http://browshot.com/api/documentation#thumbnails for the full list of possible arguments.
+	# 
+	# +url+:: URL of the screenshot (screenshot_url value retrieved from screenshot_create() or screenshot_info()). You will get the full image if no other argument is specified.
+	# +file+:  Local file name to write to.
 	def screenshot_thumbnail_file(url='', file='', parameters={})
 		content = screenshot_thumbnail(url, parameters);
 
@@ -96,7 +138,10 @@ class Browshot
 		end
 	end
 
-
+	#  Return information about the user account. See http://browshot.com/api/documentation#account_info for the response format.
+	def account_info(parameters={})
+		return return_reply('account/info', parameters)
+	end
 
 	private
 
